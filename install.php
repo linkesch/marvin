3a1b596ceccf032ad115d9cd3e4c11115670052c
Pavel Linkesch <pavel.linkesch@gmail.com>
2014-08-19 16:11:44 +0200

    move default theme installation to plugins

--- install.php
+++ install.php
@@ -6,31 +6,37 @@ use Composer\Script\Event;
 
 class Install
 {
-  public static function postPackageInstall(Event $event)
-  {
-    $installedPackage = $event->getOperation()->getPackage();
-    if ($installedPackage->getName() != 'marvin/marvin') {
-      return false;
+    public static function postPackageInstall(Event $event)
+    {
+        $installedPackage = $event->getOperation()->getPackage();
+        if ($installedPackage->getName() != 'marvin/marvin') {
+            return false;
+        }
+
+        $config = require 'config.php';
+
+        self::copy(__DIR__ ."/Web", $config['web_dir']);
+
+        $plugins = glob(__DIR__ .'/../*');
+        foreach ($plugins as $plugin) {
+            if (file_exists($plugin ."/Themes")) {
+                self::copy($plugin ."/Themes", $config['themes_dir']);
+            }
+        }
     }
 
-    $config = require 'config.php';
+    public static function copy($source, $dest)
+    {
+        if (!file_exists($dest)) {
+            mkdir($dest, 0755, true);
+        }
 
-    self::copy(__DIR__ ."/Web", $config['web_dir']);
-    self::copy(__DIR__ ."/Themes", $config['themes_dir']);
-  }
-
-  public static function copy($source, $dest)
-  {
-    if (!file_exists($dest)) {
-      mkdir($dest, 0755, true);
+        foreach ($iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($source, \RecursiveDirectoryIterator::SKIP_DOTS), \RecursiveIteratorIterator::SELF_FIRST) as $item) {
+            if ($item->isDir() && file_exists($dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName()) == false) {
+                mkdir($dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
+            } elseif ($item->isFile()) {
+                copy($item, $dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
+            }
+        }
     }
-
-    foreach ($iterator = new \RecursiveIteratorIterator(new \RecursiveDirectoryIterator($source, \RecursiveDirectoryIterator::SKIP_DOTS), \RecursiveIteratorIterator::SELF_FIRST) as $item) {
-      if ($item->isDir()) {
-        mkdir($dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
-      } else {
-        copy($item, $dest . DIRECTORY_SEPARATOR . $iterator->getSubPathName());
-      }
-    }
-  }
 }

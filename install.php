3e298dac02158af0aae57fdc0c9f170b9e0714df
Pavel Linkesch <pavel.linkesch@gmail.com>
2014-11-07 12:00:29 +0100

    fix composer installation

--- install.php
+++ install.php
@@ -18,6 +18,11 @@ class Install
         // Copy web dir
         self::copy(__DIR__ ."/Web", $config['web_dir']);
 
+        // Create app/ folder
+        if (file_exists($config['app_dir']) == false) {
+            mkdir($config['app_dir'], 0755);
+        }
+
         // Create app/config.php to allow installation
         $appConfig = $config['app_dir'] .'/config.php';
         if (file_exists($appConfig) == false) {
@@ -25,6 +30,10 @@ class Install
             fwrite($fp, '<?php
 
 $app["debug"] = true;
+
+$config["website"]["name"] = "Marvin";
+$config["website"]["description"] = "Marvin is a micro CMS for PHP 5.3";
+$config["website"]["url"] = "http://marvin.linkesch.sk";
 ');
             fclose($fp);
         }
